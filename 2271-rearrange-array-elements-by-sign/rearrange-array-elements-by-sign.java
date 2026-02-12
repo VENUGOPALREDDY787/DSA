a4a945c64e78f040afa62e9f5766b2b081816963
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-02-12 18:44:29 +0530

    Time: 3 ms (99.34%) | Memory: 175.4 MB (66.87%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,35 +1,17 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int evenarr[] = new int[nums.length/2];
-        int oddarr[] = new int[nums.length/2];
-        
-        int even = 0;
-        int odd = 0;
-
-        // separate positives and negatives
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > 0){
-                evenarr[even] = nums[i];
-                even++;
+        int arr[] = new int[nums.length];
+        int pos = 0;
+        int neg = 1;
+        for(int i =0; i<nums.length;i++){
+            if(nums[i]>0){
+                arr[pos]=nums[i];
+                pos+=2;
             }else{
-                oddarr[odd] = nums[i];
-                odd++;
+                arr[neg]=nums[i];
+                neg+=2;
             }
         }
-
-        // merge alternately
-        int arr[] = new int[nums.length];
-        int i = 0;
-        int k = 0;
-
-        while(i < evenarr.length){
-            arr[k] = evenarr[i];
-            k++;
-            arr[k] = oddarr[i];
-            k++;
-            i++;
-        }
-
         return arr;
     }
 }
