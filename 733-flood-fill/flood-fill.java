669e44ade9d918d29b6370ac6bfa9248eef4bced
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-02-24 17:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (64.07%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,20 +1,35 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int origanal= image[sr][sc];
-        if(origanal == color) return image;
-        fill(image,sr,sc,origanal,color);
-    return image;
-
-    }
-    public static void fill(int[][] image, int sr, int sc, int origanal,int color){
-        if(sr<0 || sr>image.length-1)return;
-        if(sc<0 || sc>image[0].length-1)return;
-        if(image[sr][sc]==origanal){
-            image[sr][sc]=color;
-            fill(image,sr+1,sc,origanal,color);
-            fill(image,sr,sc+1,origanal,color);
-            fill(image,sr-1,sc,origanal,color);
-            fill(image,sr,sc-1,origanal,color);
+        if(image[sr][sc] == color) {
+            return image;
         }
+
+        int k = image[sr][sc];
+        image[sr][sc] = color;
+        int m = image.length;
+        int n = image[0].length;
+        if(sr>0){
+            if(image[sr-1][sc] == k ) {
+                floodFill(image, sr-1, sc, color);
+            }
+        }
+        if(sc<n-1){
+            if(image[sr][sc+1] == k) {
+                floodFill(image, sr, sc+1, color);
+            }
+        }
+        if(sr<m-1){
+            if(image[sr+1][sc] == k) {
+                floodFill(image, sr+1, sc, color);
+            }
+        }
+        if(sc>0){
+            if(image[sr][sc-1] == k) {
+                floodFill(image, sr, sc-1, color);
+            }
+        }
+
+        return image;
+        
     }
 }
