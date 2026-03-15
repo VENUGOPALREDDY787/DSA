afca09df63bb55446cb1fb08246d29c2ee36583a
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-03-15 11:01:54 +0530

    Time: 19 ms (27.00%) | Memory: 46.7 MB (40.56%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,34 @@
+class Solution {
+    static int count =0;
+     static String ans = "";
+    public String getHappyString(int n, int k) { 
+         count = 0;
+        ans = "";
+
+        getthekthString(n,k,'a',"");
+        getthekthString(n, k, 'b', "");
+        getthekthString(n, k, 'c', "");
+        return ans;
+    }
+    public static void getthekthString(int n,int k ,char curr,String str){
+        str+=curr;
+        if(str.length() == n){
+           count++;
+           if(count == k){
+            ans = str;
+            } 
+            return;
+        }
+        if(curr == 'a'){
+            getthekthString(n,k,'b',str);
+            getthekthString(n,k,'c',str);
+        }else if(curr =='b'){
+            getthekthString(n,k,'a',str);
+            getthekthString(n,k,'c',str); 
+        }else{
+             getthekthString(n,k,'a',str);
+              getthekthString(n,k,'b',str);
+        }
+        
+    }
+}
