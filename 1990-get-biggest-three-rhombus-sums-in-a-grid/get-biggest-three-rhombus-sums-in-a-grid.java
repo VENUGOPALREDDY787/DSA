021946690a1a60ca831df78d963f57195e37fde5
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-03-16 13:51:40 +0530

    Time: 17 ms (98.53%) | Memory: 47.2 MB (83.82%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
@@ -1,76 +1,98 @@
 class Solution {
+
     static int[] values = new int[3];
+
     public int[] getBiggestThree(int[][] grid) {
-        for(int i =0;i<3;i++){
-            values[i]=0;  
+
+        for (int i = 0; i < 3; i++) {
+            values[i] = 0;
         }
+
         int n = grid.length;
         int m = grid[0].length;
-        
-        for(int i = 0 ; i<grid.length;i++){
-            for(int j = 0 ;j<grid[0].length;j++){
-                int maxsize = Math.min(i,Math.min(j,Math.min(n-1-i,m-j-1)));
-                if(maxsize == 0){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                int maxsize = Math.min(i, Math.min(j, Math.min(n - 1 - i, m - j - 1)));
+
+                if (maxsize == 0) {
                     arrange(grid[i][j]);
                 }
-               for(int k = 1;k<=maxsize;k++){
-                    int val = findsum(i,j,k,grid);
+
+                for (int k = 1; k <= maxsize; k++) {
+                    int val = findsum(i, j, k, grid);
                     arrange(val);
                 }
             }
         }
-      int count = 0;
-for(int i = 0; i < 3; i++){
-    if(values[i] != 0) count++;
-}
 
-int[] ans = new int[count];
-for(int i = 0; i < count; i++){
-    ans[i] = values[i];
-}
-return ans;
-        
-    }
-   public static int findsum(int i , int j , int k,int[][] grid){
+        int count = 0;
 
-    int sum = 0;
-    int top = i-k;
-    int bottom = i+k;
-
-    // top half
-    for(int l = 0; l <= k; l++){
-        if(l == 0){
-            sum += grid[top][j];
-        }else{
-            sum += grid[top + l][j - l] + grid[top + l][j + l];
-        }
-    }
-
-    // bottom half
-    for(int l = 1; l < k; l++){
-        sum += grid[bottom - l][j - l] + grid[bottom - l][j + l];
-    }
-
-    sum += grid[bottom][j];
-
-    return sum;
-}
-    public static void arrange(int val){
-
-  
-    for(int i = 0; i < 3; i++){
-        if(values[i] == val) return;
-    }
-
-   
-    for(int i = 0; i < 3; i++){
-        if(val > values[i]){
-            for(int j = 2; j > i; j--){
-                values[j] = values[j-1];
+        for (int i = 0; i < 3; i++) {
+            if (values[i] != 0) {
+                count++;
             }
-            values[i] = val;
-            break;
+        }
+
+        int[] ans = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            ans[i] = values[i];
+        }
+
+        return ans;
+    }
+
+    public static int findsum(int i, int j, int k, int[][] grid) {
+
+        int sum = 0;
+
+        int top = i - k;
+        int bottom = i + k;
+
+        // top half
+        for (int l = 0; l <= k; l++) {
+
+            if (l == 0) {
+                sum += grid[top][j];
+            } 
+            else {
+                sum += grid[top + l][j - l] + grid[top + l][j + l];
+            }
+        }
+
+        // bottom half
+        for (int l = 1; l < k; l++) {
+            sum += grid[bottom - l][j - l] + grid[bottom - l][j + l];
+        }
+
+        sum += grid[bottom][j];
+
+        return sum;
+    }
+
+    public static void arrange(int val) {
+
+        // avoid duplicates
+        for (int i = 0; i < 3; i++) {
+            if (values[i] == val) {
+                return;
+            }
+        }
+
+        // maintain top 3 values
+        for (int i = 0; i < 3; i++) {
+
+            if (val > values[i]) {
+
+                for (int j = 2; j > i; j--) {
+                    values[j] = values[j - 1];
+                }
+
+                values[i] = val;
+                break;
+            }
         }
     }
-}
 }
