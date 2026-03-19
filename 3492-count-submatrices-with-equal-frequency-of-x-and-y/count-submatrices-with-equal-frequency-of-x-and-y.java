0e2604dfaae1c457d92496342ccfae709de08169
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-03-19 11:12:29 +0530

    Time: 16 ms (100.00%) | Memory: 260.3 MB (7.69%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -2,27 +2,38 @@ class Solution {
     public int numberOfSubmatrices(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+
         int[] x = new int[m];
         int[] y = new int[m];
+
         int count = 0;
-        for(int i = 0; i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j] =='X'){
+
+        for (int i = 0; i < n; i++) {
+
+            // Update column-wise counts
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 'X') {
                     x[j]++;
-                }if(grid[i][j] =='Y'){
+                }
+                if (grid[i][j] == 'Y') {
                     y[j]++;
                 }
             }
+
             int xsum = 0;
             int ysum = 0;
-            for(int j = 0;j<m;j++){
-                xsum+=x[j];
-                ysum+=y[j];
-                if(xsum!=0 && (ysum!= 0 && xsum== ysum)){
+
+            // Check prefix sums
+            for (int j = 0; j < m; j++) {
+                xsum += x[j];
+                ysum += y[j];
+
+                if (xsum != 0 && ysum != 0 && xsum == ysum) {
                     count++;
                 }
             }
         }
+
         return count;
     }
 }
