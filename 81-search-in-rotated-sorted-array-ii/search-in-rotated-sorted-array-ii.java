a5e5403c00905f39e702c3567132288701bf253e
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-04-09 23:59:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (85.22%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int si = 0;
+        int ei = nums.length - 1;
+
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            // Handle duplicates
+            if (nums[si] == nums[mid] && nums[mid] == nums[ei]) {
+                si++;
+                ei--;
+            }
+            // Left half is sorted
+            else if (nums[si] <= nums[mid]) {
+                if (nums[si] <= target && target < nums[mid]) {
+                    ei = mid - 1;
+                } else {
+                    si = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[ei]) {
+                    si = mid + 1;
+                } else {
+                    ei = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
