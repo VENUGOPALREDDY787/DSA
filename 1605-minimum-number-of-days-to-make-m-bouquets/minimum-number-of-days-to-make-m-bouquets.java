ecf015d3c2d08080f2a6913bc0e459115d32dc45
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-04-14 00:31:38 +0530

    Time: 13 ms (99.66%) | Memory: 82.4 MB (85.00%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,43 +1,41 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        if ((long) m * k > bloomDay.length) {
+        int n = bloomDay.length;
+        if (n < (long)m * k)
             return -1;
-        }
-
-        int low = 1, high = (int) 1e9;
+        int high = (int) 1e9;
+        int low = 1;
         while (low < high) {
             int mid = low + (high - low) / 2;
-
-            if (isPossibleBouquets(bloomDay, m, k, mid)) {
+            if (canpossable(bloomDay, m, k, mid)) {
                 high = mid;
             } else {
                 low = mid + 1;
             }
-        };
-
+        }
         return low;
     }
-    private boolean isPossibleBouquets(int[] bloomDay, int m, int k, int day) {
-        int total = 0;
 
+    public static boolean canpossable(int[] bloomDay, int m, int k, int day) {
+        int total = 0;
         for (int i = 0; i < bloomDay.length; i++) {
             int count = 0;
-            while (i < bloomDay.length && count < k && bloomDay[i] <= day) {
+            while (i < bloomDay.length && bloomDay[i] <=day && count < k) {
                 count++;
                 i++;
             }
-
             if (count == k) {
                 total++;
                 i--;
             }
-
-            if (total >= m) {
-                return true;
-            }
+            
+            if (total >= m) return true;
         }
-
         return false;
     }
-
 }
+
+
+
+
+
