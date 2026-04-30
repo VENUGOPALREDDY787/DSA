2ca13418c9a2b4de7fa88d136a1d18c3109d0d2a
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-04-30 23:30:05 +0530

    Time: 5 ms (98.36%) | Memory: 44.1 MB (26.60%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,27 +1,15 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length())return false;
-        HashMap<Character,Character> map = new HashMap<>();
-        HashMap<Character,Character> reverseMap = new HashMap<>();
-        for(int i =0; i< s.length();i++){
-            char ch = s.charAt(i);
-            char ch2 = t.charAt(i);
-            if(map.containsKey(ch)){
-                if(map.get(ch) != ch2){
-                    return false;
-                }
-            }else{
-                map.put(ch,ch2);
+        int[] maps = new int[256];
+        int[] mapt = new int[256];
+        for(int i = 0; i<s.length();i++){
+            char chs = s.charAt(i);
+            char cht = t.charAt(i);
+            if(maps[chs]!=mapt[cht]){
+                return false;
             }
-
-             if(reverseMap.containsKey(ch2)){
-                if(reverseMap.get(ch2) != ch){
-                    return false;
-                }
-            }else{
-                reverseMap.put(ch2,ch);
-            }
-
+            maps[chs]=i+1;
+            mapt[cht] = i+1;
         }
         return true;
     }
