5f5692af343dcad2a1b4d61a050c51a8f01f96ec
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-05-23 20:59:04 +0530

    Time: 3 ms (99.89%) | Memory: 46.5 MB (77.29%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,55 +1,26 @@
-import java.util.*;
-
 class Solution {
-
-    class node implements Comparable<node> {
-
-        int count;
-        char ch;
-
-        public node(char ch, int count) {
-            this.count = count;
-            this.ch = ch;
-        }
-
-        @Override
-        public int compareTo(node n) {
-            return n.count - this.count;
-        }
-    }
-
     public String frequencySort(String s) {
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        // count frequency
-        for (int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        int[] freq = new int[128];
+        char[] arr = s.toCharArray();
+        for(char ch: arr){
+            freq[ch]++;
         }
-
-        PriorityQueue<node> pq = new PriorityQueue<>();
-
-        for (char ch : map.keySet()) {
-            pq.add(new node(ch, map.get(ch)));
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        while (!pq.isEmpty()) {
-
-            node now = pq.poll();
-
-            int count = now.count;
-
-            while (count > 0) {
-                sb.append(now.ch);
-                count--;
+        char[] res = new char[s.length()];
+        int idx = 0;
+        while(idx<s.length()){
+            int maxlen =0;
+            int maxidx = 0;
+            for(int i = 0;i< freq.length;i++){
+                if(freq[i]>maxlen){
+                    maxlen = freq[i];
+                    maxidx = i;
+                }
+            }
+            while(freq[maxidx]>0){
+                res[idx++] = (char)maxidx;
+                freq[maxidx]--;
             }
         }
-
-        return sb.toString();
+        return new String(res);
     }
 }
