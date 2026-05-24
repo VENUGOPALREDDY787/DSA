5a147b2356e1f201e2f9021290bf3abf9b524e4c
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-05-24 11:04:47 +0530

    Time: 6 ms (99.11%) | Memory: 47.6 MB (89.45%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.java
+++ 2001-jump-game-vii/jump-game-vii.java
@@ -1,20 +1,25 @@
 class Solution {
     public boolean canReach(String s, int minJump, int maxJump) {
         int n = s.length();
-        if(s.charAt(n-1)=='1')return false;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        int maxreach = 0;
-        while(!q.isEmpty()){
-            int i = q.remove();
-            if(i== n-1)return true;
-            for(int j = Math.max(i+minJump,maxreach);j<=Math.min(i+maxJump,n-1);j++){
-                if(s.charAt(j) =='0'){
-                    q.add(j);
-                }
+        if(s.charAt(n-1) == '1')return false;
+        boolean[] dp = new boolean[n];
+        dp[0] = true;
+        int min = 0;
+        for(int i = 0; i<n;i++){
+            if(!dp[i]){
+                continue;
             }
-            maxreach = Math.min(i+maxJump+1,n-1);
+                min =Math.max(i+minJump,min);
+                int max = Math.min(i+maxJump,n-1);
+                for(int j = min; j<=max;j++){
+                    if(s.charAt(j) == '0'){
+                    dp[j] = true;
+                    }
+                }
+                min = Math.min(max,n-1);
+             if(dp[n-1] == true)return true;
         }
+       
         return false;
     }
 }
