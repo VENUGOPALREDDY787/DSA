49a5ad4fc3e3e9979790f6524613ede5fae6b8de
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-05-25 22:33:57 +0530

    Time: 2 ms (87.74%) | Memory: 47.5 MB (94.61%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int max=0;
+        if(n==1)return true;
+        for(int i = 0; i<n-1;i++){
+            if(i>max)return false;
+            max = Math.max(max,i+nums[i]);
+            if(max>=n-1)return true;
+        }
+        return false;
+    }
+}
