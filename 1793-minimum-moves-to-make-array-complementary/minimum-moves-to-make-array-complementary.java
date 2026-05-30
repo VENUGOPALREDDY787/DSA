6e0ecfa63ed6e7ceb6702654ee4c911bf543c4dc
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-05-30 22:30:20 +0530

    Time: 6 ms (99.17%) | Memory: 76.4 MB (37.91%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.java
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.java
@@ -1,47 +1,34 @@
 class Solution {
     public int minMoves(int[] nums, int limit) {
-        int len = 2 * limit + 2;
-        int ber = nums.length / 2;
+        int[] arr = new int[2 * limit + 2];
         int n = nums.length;
 
-        int[] arr = new int[len];
-        
-
-        for (int i = 0; i < ber; i++) {
+        for (int i = 0; i < n / 2; i++) {
             int a = nums[i];
             int b = nums[n - 1 - i];
 
-            arr[0] += 2;
-            arr[len - 1] -=2;
+            int low = Math.min(a, b) + 1;
+            int high = Math.max(a, b) + limit;
+            int sum = a + b;
 
-            int min = Math.min(a, b) + 1;
-            int max = Math.max(a, b) + limit;
+            arr[2] += 2;
+            arr[2 * limit + 1] -= 2;
 
-            arr[min]--;
-            arr[max + 1]++;
+            arr[low]--;
+            arr[high + 1]++;
 
-            arr[a + b]--;
-            arr[a + b + 1]++;
+            arr[sum]--;
+            arr[sum + 1]++;
         }
 
-        int result = Integer.MAX_VALUE;
-        int[] arr2 = new int[len];
+        int ans = Integer.MAX_VALUE;
+        int curr = 0;
 
-        for (int j = 0; j < len; j++) {
-            
-
-          
-            if (j == 0) {
-                arr2[j] = arr[j];
-            } else {
-                arr2[j] = arr2[j - 1] + arr[j];
-            }
-
-            if (j >= 2 && j <= 2 * limit) {
-                result = Math.min(result, arr2[j]);
-            }
+        for (int s = 2; s <= 2 * limit; s++) {
+            curr += arr[s];
+            ans = Math.min(ans, curr);
         }
 
-        return result;
+        return ans;
     }
 }
