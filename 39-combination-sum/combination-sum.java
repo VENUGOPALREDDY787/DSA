9e661f434fe99bca3768651c2909cb74be85b379
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-06-04 11:22:05 +0530

    Time: 12 ms (4.43%) | Memory: 46.2 MB (8.05%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,24 +1,25 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> al=new ArrayList<>();
-        List<Integer> li=new ArrayList<>();
-        func(al,li,target,candidates,0);
-        return al;
-
+    
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+       HashSet<List<Integer>> set  = new HashSet<>();
+        List<List<Integer>> ml = new ArrayList<>();
+        List<Integer> li = new ArrayList<>();
+        helper(ml,li,0,target,nums,set);
+        return ml;
     }
-    public static void func(List<List<Integer>> al,List<Integer> li,int target,int[] candidates,int indx){
-        if(target==0){
-            al.add(new ArrayList<>(li));
-            return;
+    public static void helper(List<List<Integer>> ml, List<Integer> li, int idx ,int target,int[] nums,HashSet<List<Integer>> set ){
+        if(idx == nums.length|| target<0)return;
+        if(target == 0 && !set.contains(li)){
+            ml.add(new ArrayList<>(li));
+            
+                
+                set.add(new ArrayList<>(li));
+            
         }
-        for(int i=indx;i<candidates.length;i++){
-            if(candidates[i]<=target){
-                li.add(candidates[i]);
-                func(al,li,target-candidates[i],candidates,i);
-                li.remove(li.size()-1);
-            }
-        }
-
-
+        li.add(nums[idx]);
+        helper(ml,li,idx,target-nums[idx],nums,set);
+        helper(ml,li,idx+1,target-nums[idx],nums,set);
+        li.remove(li.size()-1);
+        helper(ml,li,idx+1,target,nums,set);
     }
 }
