1c995c138089a147aeb6efb99be398469112661e
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-06-30 22:38:41 +0530

    Time: 48 ms (17.54%) | Memory: 46.6 MB (72.25%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,27 +1,31 @@
 class Solution {
-    static int count;
-    public int removeStones(int[][] stones) {
-        count = 0;
-        int n = stones.length;
-        boolean vis[] = new boolean[n];
-        for(int i = 0; i<n; i++){
-            if(!vis[i]){
-                count--;
-            dfs(stones,i,vis);
-            }
-        }
-        return count;
-    }
-    public void dfs(int[][] stones, int i,boolean vis[]){
-        vis[i] = true;
+  int count = 0;
+
+  public int removeStones(int[][] stones) {
+    int n = stones.length;
+
+    boolean[] visited = new boolean[n];
+
+    for (int i = 0; i < n; i++) {
+      if (!visited[i]) {
+        dfs(visited, i, stones);
         count++;
-        for(int j = 0;j<stones.length;j++){
-            int x= stones[j][0];
-            int y = stones[j][1];
-            if(!vis[j] && (x==stones[i][0] || y == stones[i][1])){
-                dfs(stones,j,vis);
-            }
-        }
+
+      }
     }
-};
- 
+    return n - count;
+
+  }
+
+  public void dfs(boolean[] visited, int node, int[][] stones) {
+    visited[node] = true;
+
+    for (int i = 0; i < stones.length; i++) {
+      if (!visited[i] && (stones[node][0] == stones[i][0] || stones[node][1] == stones[i][1])) {
+
+        dfs(visited, i, stones);
+
+      }
+    }
+  }
+}
