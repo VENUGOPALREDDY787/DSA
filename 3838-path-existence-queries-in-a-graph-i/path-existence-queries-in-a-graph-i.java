b5ec07cd31f787c80603adb655d2e4963cb4308c
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-07-09 14:34:09 +0530

    Time: 3 ms (97.03%) | Memory: 163 MB (77.23%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -1,57 +1,22 @@
 class Solution {
-    public static int[] rank;
-    public static int[] par;
-
-    public int find(int x) {
-        if (par[x] != x) {
-            par[x] = find(par[x]); // Path Compression
-        }
-        return par[x];
-    }
-
-    public void union(int x, int y) {
-        int parX = find(x);
-        int parY = find(y);
-
-        if (parX == parY) return;
-
-        if (rank[parX] == rank[parY]) {
-            par[parY] = parX;
-            rank[parX]++;
-        } else if (rank[parX] < rank[parY]) {
-            par[parX] = parY;
-        } else {
-            par[parY] = parX;
-        }
-    }
-
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        rank = new int[n];
-        par = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            par[i] = i;
-            rank[i] = 1;
+        int batchId  = 0;
+        int[] batch = new int[n];
+        batch[0] = batchId;
+        for(int i = 1;i<n;i++){
+            if(nums[i]-nums[i-1]>maxDiff){
+                batchId++;
+            }
+            batch[i] = batchId;
         }
-
-        // Build DSU
-        for (int i = 0; i < n-1; i++) {
-           if(nums[i+1]-nums[i]<=maxDiff){
-            union(i,i+1);
-           }
-        }
-
         boolean[] res = new boolean[queries.length];
-
-        for (int i = 0; i < queries.length; i++) {
+        for(int i = 0; i<queries.length;i++){
             int u = queries[i][0];
             int v = queries[i][1];
-
-            if (find(u) == find(v)) {
+            if(batch[u] == batch[v]){
                 res[i] = true;
             }
         }
-
         return res;
     }
 }
