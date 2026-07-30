ec1b3887c21d8355b961f3913feed0a1a8ae446c
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-07-30 19:28:39 +0530

    Time: 47 ms (57.56%) | Memory: 47.5 MB (54.27%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -7,17 +7,20 @@ class Solution {
        } 
        if(sum%2 != 0)return false;
        sum/=2;
-       int[][] dp= new int[n+1][sum+1];
+       boolean[][] dp= new boolean[n+1][sum+1];
+       for(int i = 0; i<=n;i++){
+        dp[i][0] = true;
+       }
        for(int i = 1;i<=n;i++){
         for(int j = 1;j<=sum;j++){
             int v = nums[i-1];
             if(j-v>=0){
-                dp[i][j] = Math.max(v+dp[i-1][j-v],dp[i-1][j]);
+                dp[i][j] = dp[i-1][j]||dp[i-1][j-v]?true:false;
             }else{
                 dp[i][j] = dp[i-1][j];
             }
         }
        } 
-       return dp[n][sum] == sum?true:false;
+       return dp[n][sum];
     }
 }
