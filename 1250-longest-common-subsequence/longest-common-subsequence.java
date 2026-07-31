fbdcda203bc0b433e15e3f8ddabf822e600f75c7
VENUGOPALREDDY787 <venugopalreddy1895@gmail.com>
2026-07-31 11:06:15 +0530

    Time: 20 ms (78.38%) | Memory: 53.8 MB (70.10%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,21 +2,15 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i = 0; i<=n;i++){
-            for(int j = 0;j<=m;j++){
-                dp[i][j] = -1;
+        int dp[][] = new int[n+1][m+1];
+        for(int i = 1; i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(text1.charAt(i-1)== text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                }
             }
-        }
-        return slove(n-1,m-1,text1,text2,dp);
-    }
-    public int slove(int n,int m ,String text1,String text2,int[][] dp){
-        if(n < 0 || m < 0)return 0;
-        if(dp[n][m] !=-1)return dp[n][m];
-        if(text1.charAt(n) == text2.charAt(m)){
-            dp[n][m] = 1+slove(n-1,m-1,text1,text2,dp);
-        }else{
-            dp[n][m] = Math.max(slove(n-1,m,text1,text2,dp),slove(n,m-1,text1,text2,dp));
         }
         return dp[n][m];
     }
